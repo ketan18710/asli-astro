f06d740eb6c4e92cb230c4c55a7b584db33410b7
ANKIT KUMAR <work.ankit2@gmail.com>
2024-12-24 15:05:29 +0530

    Update scripts.js
    added arrow functionality code

--- scripts.js
+++ scripts.js
@@ -34,6 +34,69 @@
 // });
 
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   const burgerMenu = document.querySelector(".burger-menu");
+//   const mobileMenu = document.querySelector(".mobile-menu");
+
+//   burgerMenu.addEventListener("click", function () {
+//     mobileMenu.classList.toggle("active");
+//   });
+// });
+// $(document).ready(function () {
+//   $(".slider1").slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     dots: true,
+//     arrows: false,
+//     responsive: [
+//       {
+//         breakpoint: 900,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//     ],
+//     // dotsClass: "w-0.5 h-0.5",
+//   });
+//   $("#burger-menu-btn").on("click", function () {
+//     $("#burger-menu").toggleClass("hidden");
+//   });
+
+//   $("#burger-menu_close_btn").on("click", function () {
+//     $("#burger-menu").addClass("hidden");
+//   });
+
+//   // Initialize testimonials slider
+//   $('.testimonials-slider').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+//     nextArrow: '<button type="button" class="slick-next">Next</button>',
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           centerMode: true,
+//           centerPadding: '20px'
+//         }
+//       }
+//     ]
+//   });
+// });
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   const burgerMenu = document.querySelector(".burger-menu");
   const mobileMenu = document.querySelector(".mobile-menu");
@@ -68,14 +131,16 @@ $(document).ready(function () {
     $("#burger-menu").addClass("hidden");
   });
 
-  // Initialize testimonials slider
+  // Initialize testimonials slider with updated options
   $('.testimonials-slider').slick({
     dots: true,
     infinite: true,
-    speed: 300,
+    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
+    centerMode: true,
+    centerPadding: '0',
     prevArrow: '<button type="button" class="slick-prev">Previous</button>',
     nextArrow: '<button type="button" class="slick-next">Next</button>',
     responsive: [
@@ -85,9 +150,42 @@ $(document).ready(function () {
           slidesToShow: 1,
           slidesToScroll: 1,
           centerMode: true,
-          centerPadding: '20px'
+          centerPadding: '0',
+          arrows: true,
+          dots: true
         }
       }
     ]
+  });
+
+  $('.testimonials-slider .slider-container').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  // Custom arrow functionality
+  $('.prev-arrow').click(function(){
+    $('.testimonials-slider .slider-container').slick('slickPrev');
+  });
+
+  $('.next-arrow').click(function(){
+    $('.testimonials-slider .slider-container').slick('slickNext');
   });
 });
